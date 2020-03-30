7aa7adc1d91369a71cf71d2b53fb81acc00ca07a
amiruzg134 <amiruzg@gmail.com>
2020-03-30 14:11:34 +0700

    HP

---
+++ berita/berita-16.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tahura Raden Soerjo</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/logo-tahura.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+</head>
+
+<body>
+    <div class="preloader"></div><!-- /.preloader -->
+
+    <div class="page-wrapper">
+        <header class="site-header header-one header-one__home-two">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box clearfix">
+                        <a class="navbar-brand" href="../index.html">
+                            <img src="../images/logos/logos.svg" class="main-logo" alt="Awesome Image" />
+                        </a>
+                        <button class="menu-toggler" data-target=".header-one .main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box one-page-scroll-menu ">
+                            <li class="scrollToLink">
+                                <a href="../index.html">Beranda</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../index.html">Profil</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../sekilas.html">Pelajari Tahura</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../sekilas.html">Tugas dan Fungsi</a></li>
+                                    <li><a href="../organisasi.html">Struktur Organisasi</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.header-one -->
+
+        <section class="inner-banner">
+            <div class="container text-center">
+                <ul class="thm-breadcrumb">
+                    <li class="thm-breadcrumb__item"><a href="../index.html">Beranda</a></li>
+                    <li class="thm-breadcrumb__item"><span>Berita</span></li>
+                </ul><!-- /.thm-breadcrumb -->
+            </div><!-- /.container -->
+        </section><!-- /.inner-banner -->
+
+        <section class="blog-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="blog-details__content">
+                            <div class="blog-details__image">
+                                <img src="../images/berita/img-33.jpg" alt="Awesome Image" />
+                                <div class="blog-details__date">28 Maret 2020</div>
+                            </div>
+                            <div class="blog-details__meta">
+                                <a href="#" class="blog-details__meta-link">By Admin</a>
+                            </div>
+                            <h3 class="blog-details__title">Paok pancawarna <i>(Pitta guajana)</i></h3>
+                            <p class="blog-details__text">
+                                Paok pancawarna (bahasa Latin: <i>Pitta guajana</i>) adalah spesies burung dari keluarga Pittidae, dari genus Pitta. Burung ini merupakan jenis burung pemakan semut, kecoa, kumbang, siput, cacing, rayap, ula yang memiliki habitat di hutan primer, hutan sekunder tertutup, tersebar sampai ketinggian 1.500 m dpl. Di kawasan Tahura Raden Soerjo, burung ini bisa ditemui di blok sendi, blok lemah bang, dan blok cangar. Burung pok hijau pada umumnya hidup pada hutan yang mempunyai kelembaban yang baik. Dengan demikian, burung ini sangat menyukai hutan basah untuk dijadikan sebagai tempat tinggalnya seperti pada kawasan Tahura Raden Soerjo.
+                            </p>
+                            <p class="blog-details__text">
+                                Kebiasaan dari burung paok hijau pada saat hidup di hutan yaitu lebih banyak berjalan dengan melompat-lompat di atas tanah dari pada terbang mengitari pepohonan. Burung paok hijau yang melompat-lompat pada bagian tanah juga tidak termasuk suatu suatu rutinitas yang dilakukan tanpa alasan.
+                            </p>
+                            <p class="blog-details__text">
+                                Selain itu burung ini juga akan mencari buah-buahan yang jatuh dari pohon. Ia akan mematuki kayu yang telah mati untuk dapat memakan invertebrata dan ulat-ulat kecil yang bersembunyi di balik kayu yang mati. Burung paok hijau ini juga aktif pada siang hari. Sedangkan pada saat malam hari burung ini akan beristirahat di atas ranting pohon.
+                            </p>
+                            <p class="blog-details__text">
+                               <h3>Klasifikasi Ilmiah</h3>     
+                               <ul>
+                                   <li>Kingdom : Animalia</li>
+                                   <li>Filum : Chordata</li>
+                                   <li>Kelas : Aves</li>
+                                   <li>Ordo : Passeriformes</li>
+                                   <li>Famili : Pittidae</li>
+                                   <li>Genus : Pitta</li>
+                                   <li>Spesies : P. guajana</li>
+                               </ul>                       
+                            </p>
+                            <p class="blog-details__text">
+                                <h3>Ciri-ciri</h3>
+                                Paok pancawarna memiliki tubuh berukuran sedang (22 cm). Tubuh gemuk, berwarna keemasan bergaris-garis. Kepala hitam dengan alis kuning mencolok dan khas. Punggung dan sayap coklat dengan garis sayap putih, Ekor biru, Dagu putih. Ras Jawa: dada dan sisi lambung bergaris-garis hitam dan kuning.
+                            </p>
+                            <p class="blog-details__text">
+                                Burung jantan: ada garis biru pada dada atas. Burung betina: lebih suram dan lebih merah. Iris coklat, paruh dan kaki hitam. Bersifat pemalu, kadang terlihat melintas jalan di hutan, namun lebih sering terdengar suaranya. Berlompatan cepat di sepanjang lantai hutan atau batang mati. Kadang di semak rendah, pohon salak atau rotan. Malam hari bertengger pada vegetasi rendah setinggi 1-3 m dari tanah.
+                            </p>
+                            <p class="blog-details__text">
+                                Sarang berbentuk bola berongga dari vegetasi pada semak, satu meter di atas permukaan tanah. Telur berwarna keputih-putihan, jumlah 3-5 butir. Berbiak bulan Januari-Maret, September-Oktober.
+                            </p>
+                            <br>
+                            <a href="../berita-all.html" class="banner-one__btn" style="display: table; margin: 0 auto">Berita Lainnya</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <?php include 'sidebar.html'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="site-footer" id="kontak" style="padding-top: 150px">
+            <div class="site-footer__main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget footer-widget--about-widget">
+                                <a href="../index.html" class="footer-widget__footer-logo"><img src="../images/logos/logos2.png" alt="Awesome Image" /></a>
+                                <p>Jl. Simpang Panji Suroso No.144, Arjosari, Kec. Blimbing, <br>
+                                    Kota Malang, Jawa Timur 65126</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Kontak
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>(0341) – 483254</li>
+                                    <li>tahuraradensoerjo@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Jam Operasional
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>Senin - Kamis, 07.00 - 15.30</li>
+                                    <li>Jumat, 07.00 - 14.30</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 d-flex">
+                            <div class="footer-widget my-auto">
+                                <div class="social-block">
+                                    <a target="_blank" href="https://instagram.com/tahuraradensoerjo.official?igshid=1lwx5yb6lhu3b"><i class="fab fa-instagram fa-2x"></i></a>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCl0gtcFpoFrQY6MU50tqC4Q?view_as=subscriber"><i class="fab fa-youtube fa-2x"></i></a>
+                                    <a target="_blank" href="mailto:tahuraradensoerjo@gmail.com?Subject=Hello"><i class="fas fa-envelope fa-2x"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="site-footer__bottom-footer text-center">
+                <div class="container">
+                    <p>&copy; copyright 2020 by <a href="index.html">UPT Tahura Raden Soerjo</a></p>
+                    <p>by <a target="_blank" href="https://www.instagram.com/amiruzg/">Amiruzzuhhad Gunes</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- /.scroll-to-top -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.fancybox.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <script src="../js/theme.js"></script>
+</body>
+
+</html>
