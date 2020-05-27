4c9e988ce5f00f03f0fe428d3545018d62abf68a
amiruzg134 <amiruzg@gmail.com>
2020-05-27 13:30:18 +0700

    this is a new commit

---
+++ berita/berita-17.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tahura Raden Soerjo</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/logo-tahura.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+</head>
+
+<body>
+    <div class="preloader"></div><!-- /.preloader -->
+
+    <div class="page-wrapper">
+        <header class="site-header header-one header-one__home-two">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box clearfix">
+                        <a class="navbar-brand" href="../index.html">
+                            <img src="../images/logos/logos.svg" class="main-logo" alt="Awesome Image" />
+                        </a>
+                        <button class="menu-toggler" data-target=".header-one .main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box one-page-scroll-menu ">
+                            <li class="scrollToLink">
+                                <a href="../index.html">Beranda</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../index.html">Profil</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../sekilas.html">Pelajari Tahura</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../sekilas.html">Tugas dan Fungsi</a></li>
+                                    <li><a href="../organisasi.html">Struktur Organisasi</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.header-one -->
+
+        <section class="inner-banner">
+            <div class="container text-center">
+                <ul class="thm-breadcrumb">
+                    <li class="thm-breadcrumb__item"><a href="../index.html">Beranda</a></li>
+                    <li class="thm-breadcrumb__item"><span>Berita</span></li>
+                </ul><!-- /.thm-breadcrumb -->
+            </div><!-- /.container -->
+        </section><!-- /.inner-banner -->
+
+        <section class="blog-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="blog-details__content">
+                            <div class="blog-details__image">
+                                <div class="blog-details__date">15 April 2020</div>
+                            </div>
+                            <div class="blog-details__meta">
+                                <a href="#" class="blog-details__meta-link">By hildalho</a>
+                            </div>
+                            <h3 class="blog-details__title">Rebung, Salah Satu Potensi Yang Menimbulkan Kerawanan Keamanan Kawasan Tahura Raden Soerjo</i></h3>
+                            <p class="blog-details__text">
+                                Rebung atau tunas bambu, sangat terkenal di kalangan masyarakat Indonesia sebagai bahan makanan. Bahkan Kota Semarang yang terkenal dengan kuliner lumpianya menggunakan olahan rebung sebagai bahan isiannya. Rebung tidak hanya digunakan sebagai bahan makanan, tetapi juga dapat digunakan sebagai pemacu tumbuh tanaman karena dalam rebung terkandung zat pengatur tumbuh yaitu hormon giberelin. Beberapa penelitian terhadap rebung, terbukti dapat meningkatkan produksi padi di lahan gambut, meningkatkan pertumbuhan diameter, tinggi dan berat akar pada bibit Sengon serta pada bibit Kelapa Sawit.  
+                            </p>
+                            <p class="blog-details__text">
+                                Panen raya rebung terjadi pada musim hujan, yaitu antara bulan Desember-Februari. Biasanya rebung dipanen saat tingginya telah mencapai 20 cm dari permukaan tanah, dengan diameter batang sekitar 7 cm. Pada saat itulah Tahura Raden Soerjo mengalami masa rawan gangguan keamanan, karena masyarakat sekitar kawasan Tahura di Kecamatan Gondang dan Jatirejo Kabupaten Mojokerto serta Kecamatan Wonosalam Kabupaten Jombang beramai-ramai memanen rebung dalam kawasan.
+                            </p>
+                            <p class="blog-details__text">
+                                Seharusnya masyarakat memanen rebung pada lahan-lahan masyarakat sendiri, bukan dari dalam kawasan hutan, terutama kawasan hutan konservasi Tahura Raden Soerjo yang merupakan benteng/pertahanan terakhir system penyangga kehidupan yang juga merupakan tandon air raksasa seluas 27.868,30 hektar. Namun rendahnya jumlah tanaman bambu di lahan masyarakat serta tingginya permintaan akan rebung memicu masyarakat melakukan pemanenan secara illegal dalam kawasan Tahura. 
+                            </p>
+                            <p class="blog-details__text">
+                               Kenapa rebung sangat penting bagi kawasan Tahura? Karena berdasarkan fakta-fakta, tanaman bambu bila ditinjau dari segi ekologi sangat bermanfaat bagi konservasi air dan tanah, yaitu karena kemampuannya mengikat air dan tanah. Bambu mengikat tanah pada daerah lereng, sehingga dapat mengurangi erosi, sedimentasi dan longsor; bambu mampu menyerap air hujan yang cukup besar melalui mekanisme intersepsi, sehingga kemungkinan terjadinya aliran langsung dan erosi di atas permukaan lahan dengan dominasi bambu menjadi kecil. Sementara itu, dalam kaitan dengan upaya mitigasi perubahan iklim, tanaman bambu juga dapat meningkatkan penyerapan karbon. Dari suatu penelitian menyebutkan bahwa tanaman bambu mampu menyerap lebih dari 62 ton/Ha/tahun karbon dioksida. Apabila rebung yang merupakan tanaman muda dari bambu dipanen secara besar-besaran oleh masyarakat tanpa ijin, tanpa pengaturan yang benar, maka pertumbuhan rumpun bambu dalam Tahura akan menurun, yang akan menyebabkan menurunnya fungsi tanaman sebagai pengikat air dan tanah sehingga berdampak pada menurunan fungsi kawasan.
+                            </p>
+                            <p class="blog-details__text">
+                                Tindakan masyarakat selama ini yaitu memanen secara illegal tanaman bambu dan turunannya yaitu rebung melanggar UU No. 41 Tahun 1999 tentang Kehutanan. Berdasarkan Pasal 50 ayat (3) huruf e, setiap orang dilarang memungut hasil hutan di dalam hutan tanpa memiliki hak atau izin dari pejabat yang berwenang dan ketentuan pidana apabila melakukan pelanggaran sesuai Pasal 78 ayat (6) berupa penjara paling lama 10 (sepuluh) tahun dan atau denda paling banyak 5 milyar rupiah.  
+                            </p>
+                            <p class="blog-details__text">
+                                Pada masa panen rebung petugas Tahura dibuat sibuk karena banyaknya masyarakat yang masuk dalam kawasan tanpa ijin sekaligus melakukan pelanggaran berupa memanen rebung, batang bambu dan bahkan akhir-akhir ini anakan tanaman Porang/coplok juga ikut diambil. Sejak awal tahun 2020 Resort Pengelolaan Hutan (RPH) 06 Mojokerto Barat telah menggelar patroli dengan mengerahkan seluruh personel (full team) untuk menghadang sekaligus menindak masyarakat yang tertangkap tangan melakukan pelanggaran. Para pelanggar yang turun dari hutan dengan membawa hasil hutan illegal dihadang dan diwajibkan meninggalkan hasil curiannya di dalam kawasan, diwajibkan menandatangani surat pernyataan tidak akan mengulangi perbuatannya dan mendapat pembinaan dari petugas. Selama bulan Januari 2020 saja telah tertangkap 48 orang pelaku pelanggaran sedangkan di bulan Februari 2020 tertangkap 39 orang. Pada bulan Maret 2020, patroli rutin bahkan diikuti oleh Kepala UPT Tahura, Bapak Ahmad Wahyudi, S. Hut dan berhasil menangkap 1 orang pelanggar yang membawa 1 karung plastik rebung. Patroli gabungan bekerja sama dengan Polres Mojokerto juga telah digelar sebanyak 3 kali di lokasi yang rawan pelanggaran seperti di Desa Ngembat dan Begaganlimo Kec. Gondang serta desa Rejosari Kec. Jatirejo Kabupaten Mojokerto.
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-34.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="float: left; width: 47%; margin: 5px">
+                                        <img src="../images/berita/img-34.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-35.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="float: left; width: 47%; margin: 5px">
+                                        <img src="../images/berita/img-35.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul">Pelaku pencurian rebung dalam kawasan Tahura dihadang petugas RPH 06 Mojokerto Barat di Ngembat Kec. Gondang Kab. Mojokerto menandatangani Surat Pernyataan tidak akan mengulangi perbuatannya lagi</div>
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-36.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="margin-bottom: 10px; margin-top: 30px">
+                                        <img src="../images/berita/img-36.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="max-width: 70%">Para pencuri rebung dalam kawasan Tahura rata-rata membawa 2 karung plastik besar berisi rebung hasil curiannya dengan berat perkarung kurang lebih 25 kg</div>
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-37.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="margin-bottom: 10px; margin-top: 30px">
+                                        <img src="../images/berita/img-37.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="max-width: 70%">Kepala UPT Tahura Raden Soerjo, Kepala Seksi Perlindungan dan Pemberdayaan Masyarakat bersama personel RPH 06 berhasil menghadang dan menangkap pencuri rebung yang membawa 1 karung rebung</div>
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-38.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="margin-bottom: 10px; margin-top: 30px">
+                                        <img src="../images/berita/img-38.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="max-width: 70%">Personel Tahura Raden Soerjo bersama Polres Mojokerto bersama-sama melakukan operasi gabungan dalam rangka menindak masyarakat yang melakukan pemungutan illegal/pencurian hasil hutan di kawasan Tahura</div>
+                            </p>
+                            <br>
+                            <a href="../berita-all.html" class="banner-one__btn" style="display: table; margin: 0 auto">Berita Lainnya</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <?php include 'sidebar.html'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="site-footer" id="kontak" style="padding-top: 150px">
+            <div class="site-footer__main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget footer-widget--about-widget">
+                                <a href="../index.html" class="footer-widget__footer-logo"><img src="../images/logos/logos2.png" alt="Awesome Image" /></a>
+                                <p>Jl. Simpang Panji Suroso No.144, Arjosari, Kec. Blimbing, <br>
+                                    Kota Malang, Jawa Timur 65126</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Kontak
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>(0341) – 483254</li>
+                                    <li>tahuraradensoerjo@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Jam Operasional
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>Senin - Kamis, 07.00 - 15.30</li>
+                                    <li>Jumat, 07.00 - 14.30</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 d-flex">
+                            <div class="footer-widget my-auto">
+                                <div class="social-block">
+                                    <a target="_blank" href="https://instagram.com/tahuraradensoerjo.official?igshid=1lwx5yb6lhu3b"><i class="fab fa-instagram fa-2x"></i></a>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCl0gtcFpoFrQY6MU50tqC4Q?view_as=subscriber"><i class="fab fa-youtube fa-2x"></i></a>
+                                    <a target="_blank" href="mailto:tahuraradensoerjo@gmail.com?Subject=Hello"><i class="fas fa-envelope fa-2x"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="site-footer__bottom-footer text-center">
+                <div class="container">
+                    <p>&copy; copyright 2020 by <a href="index.html">UPT Tahura Raden Soerjo</a></p>
+                    <p>by <a target="_blank" href="https://www.instagram.com/amiruzg/">Amiruzzuhhad Gunes</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- /.scroll-to-top -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.fancybox.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <script src="../js/theme.js"></script>
+</body>
+
+</html>
