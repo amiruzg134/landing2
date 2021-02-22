bad9489e17a0010dda520717245a7b640b78b294
amiruzg134 <amiruzg@gmail.com>
2021-02-22 13:50:50 +0700

    msi

---
+++ berita/berita-20.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tahura Raden Soerjo</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/logo-tahura.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+</head>
+
+<body>
+    <div class="preloader"></div><!-- /.preloader -->
+
+    <div class="page-wrapper">
+        <header class="site-header header-one header-one__home-two">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box clearfix">
+                        <a class="navbar-brand" href="../index.html">
+                            <img src="../images/logos/logos.svg" class="main-logo" alt="Awesome Image" />
+                        </a>
+                        <button class="menu-toggler" data-target=".header-one .main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box one-page-scroll-menu ">
+                            <li class="scrollToLink">
+                                <a href="../index.html">Beranda</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../index.html">Profil</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../sekilas.html">Pelajari Tahura</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../sekilas.html">Tugas dan Fungsi</a></li>
+                                    <li><a href="../organisasi.html">Struktur Organisasi</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.header-one -->
+
+        <section class="inner-banner">
+            <div class="container text-center">
+                <ul class="thm-breadcrumb">
+                    <li class="thm-breadcrumb__item"><a href="../index.html">Beranda</a></li>
+                    <li class="thm-breadcrumb__item"><span>Informasi</span></li>
+                </ul><!-- /.thm-breadcrumb -->
+            </div><!-- /.container -->
+        </section><!-- /.inner-banner -->
+
+        <section class="blog-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="blog-details__content">
+                            <div class="blog-details__image">
+                                <div class="blog-details__date">20 November 2020</div>
+                            </div>
+                            <div class="blog-details__meta">
+                                <a href="#" class="blog-details__meta-link">By Admin</a>
+                            </div>
+                            <h3 class="blog-details__title">Pemenang Lomba Fotografi Panorama Tahura Raden Soerjo</i></h3>
+                            <p class="blog-details__text">
+                                Trimakasih kepada semua sahabat Tahura Raden Soerjo yang telah mengikuti lomba foto. <br>
+
+                                Selamat ya untuk para pemenang 1,2,3 dan favorit. <br>
+
+                                Selamat kepada : <br>
+                                <ol>
+                                    <li>Juara 1 : <strong>Waluya Priya Atmaja</strong> @priy_at</li>
+                                    <li>Juara 2 : <strong>Suhartanto</strong> @_tanto17</li>
+                                    <li>Juara 3 : <strong>Ari Ristanto</strong> @ariristan</li>
+                                    <li>Juara Favorit : <strong>Nugroho Dwi Hermanto</strong> @nugroho_dwi_hermanto</li>
+                                </ol>
+                                Untuk hadiah dapat diambil di Kantor UPT Tahura Raden Soerjo, Jl. Simpang Panji Suroso Kav. 144, Arjosari, Kota Malang dengan membawa identitas/KTP asli dan tidak boleh diwakilkan. <br>
+
+                                Sekali lagi selamat untuk para pemenang👏👏🎉  
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-76.jpeg" data-fancybox="images">
+                                    <span class="img-berita" style="width: 80%; margin-bottom: 10px">
+                                        <img src="../images/berita/img-76.jpeg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="width: 80%"><strong>Juara I - WALUYA PRIYA ATMAJA</strong></div>
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-77.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="width: 80%; margin-bottom: 10px">
+                                        <img src="../images/berita/img-77.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="width: 80%"><strong>Juara II - SUHARTANTO</strong></div>
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-78.jpeg" data-fancybox="images">
+                                    <span class="img-berita" style="width: 80%; margin-bottom: 10px">
+                                        <img src="../images/berita/img-78.jpeg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="width: 80%"><strong>Juara III - ARI RISTANTO</strong></div>
+                            </p>
+                            <p class="blog-details__text">
+                                <a class="fancybox" data-fancybox-group="group" href="../images/berita/img-79.jpg" data-fancybox="images">
+                                    <span class="img-berita" style="width: 80%; margin-bottom: 10px">
+                                        <img src="../images/berita/img-79.jpg" alt="image description">
+                                    </span>
+                                </a>
+                                <div class="sub-judul" style="width: 80%"><strong>Juara Favorit - Nugroho Dwi Hermanto</strong></div>
+                            </p>
+                            
+                            <br>
+                            <a href="../berita-all.html" class="banner-one__btn" style="display: table; margin: 0 auto">Berita Lainnya</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <?php include 'sidebar.html'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="site-footer" id="kontak" style="padding-top: 150px">
+            <div class="site-footer__main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget footer-widget--about-widget">
+                                <a href="../index.html" class="footer-widget__footer-logo"><img src="../images/logos/logos2.png" alt="Awesome Image" /></a>
+                                <p>Jl. Simpang Panji Suroso No.144, Arjosari, Kec. Blimbing, <br>
+                                    Kota Malang, Jawa Timur 65126</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Kontak
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>(0341) – 483254</li>
+                                    <li>tahuraradensoerjo@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Jam Operasional
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>Senin - Kamis, 07.00 - 15.30</li>
+                                    <li>Jumat, 07.00 - 14.30</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 d-flex">
+                            <div class="footer-widget my-auto">
+                                <div class="social-block">
+                                    <a target="_blank" href="https://instagram.com/tahuraradensoerjo.official?igshid=1lwx5yb6lhu3b"><i class="fab fa-instagram fa-2x"></i></a>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCl0gtcFpoFrQY6MU50tqC4Q?view_as=subscriber"><i class="fab fa-youtube fa-2x"></i></a>
+                                    <a target="_blank" href="mailto:tahuraradensoerjo@gmail.com?Subject=Hello"><i class="fas fa-envelope fa-2x"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="site-footer__bottom-footer text-center">
+                <div class="container">
+                    <p>&copy; copyright 2020 by <a href="index.html">UPT Tahura Raden Soerjo</a></p>
+                    <p>by <a target="_blank" href="https://www.instagram.com/amiruzg/">Amiruzzuhhad Gunes</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- /.scroll-to-top -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.fancybox.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <script src="../js/theme.js"></script>
+</body>
+
+</html>
