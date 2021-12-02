156f064f17e236f48593ccba0e464abe577b112a
amiruzg134 <amiruzg@gmail.com>
2021-12-02 07:32:42 +0700

    MSI

---
+++ berita/berita-25.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tahura Raden Soerjo</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/logo-tahura.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+</head>
+
+<body>
+    <div class="preloader"></div><!-- /.preloader -->
+
+    <div class="page-wrapper">
+        <header class="site-header header-one header-one__home-two">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box clearfix">
+                        <a class="navbar-brand" href="../index.html">
+                            <img src="../images/logos/logos.svg" class="main-logo" alt="Awesome Image" />
+                        </a>
+                        <button class="menu-toggler" data-target=".header-one .main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box one-page-scroll-menu ">
+                            <li class="scrollToLink">
+                                <a href="../index.html">Beranda</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../index.html">Profil</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../sekilas.html">Pelajari Tahura</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../sekilas.html">Tugas dan Fungsi</a></li>
+                                    <li><a href="../organisasi.html">Struktur Organisasi</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.header-one -->
+
+        <section class="inner-banner">
+            <div class="container text-center">
+                <ul class="thm-breadcrumb">
+                    <li class="thm-breadcrumb__item"><a href="../index.html">Beranda</a></li>
+                    <li class="thm-breadcrumb__item"><span>Informasi</span></li>
+                </ul><!-- /.thm-breadcrumb -->
+            </div><!-- /.container -->
+        </section><!-- /.inner-banner -->
+
+        <section class="blog-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="blog-details__content">
+                            <div class="blog-details__date">22 September 2021</div>
+                            <div class="blog-details__meta">
+                                <a href="#" class="blog-details__meta-link">radarsurabaya.id</a>
+                            </div>
+                            <h3 class="blog-details__title">Pendakian Gunung Arjuno dan Welirang Mulai Dibuka untuk Umum</i></h3>
+                            <div class="blog-details__image" style="margin-bottom:30px">
+                                <img src="../images/berita/img-103.jpg" alt="Awesome Image" />
+                                <div class="sub-judul" style="padding-top:10px"><i>Gunung Arjuno</i></div>
+                            </div>
+                            <p class="blog-details__text">
+                                SURABAYA – Pendakian Gunung Arjuno-Welirang yang ada di wilayah Jawa Timur, dibuka secara bertahap, yakni pada dua pintu masuk jalur pendakian melalui Tambaksari, Kecamatan Purwodadi, dan Tretes, Kecamatan Prigen, Kabupaten Pasuruan. 
+                            </p>
+                            <p class="blog-details__text">
+                                Kepala Taman Hutan Raya (Tahura) Raden Soerjo, Ahmad Wahyudi, di Kota Malang, Jawa Timur, Selasa, mengatakan pendakian Gunung Arjuno-Welirang sudah dilakukan mulai 19 September 2021, pada dua pintu masuk tersebut.
+                            </p>
+
+                            <blockquote class="blockuot">
+                                “Untuk jalur yang dari Kota Batu, masih belum dibuka. Karena kami masih berpatokan pada instruksi Mendagri terakhir,” kata Wahyudi.
+                            </blockquote>
+
+                            <p class="blog-details__text">
+                                Wahyudi menjelaskan pembukaan pendakian Gunung Arjuno-Welirang melalui pintu masuk Tambaksari, dan Tretes yang berada di Kabupaten Pasuruan itu dikarenakan pada wilayah tersebut sudah berstatus Pemberlakuan Pembatasan Kegiatan Masyarakat (PPKM) level 2.
+                            </p>
+                            <p class="blog-details__text">
+                                Meskipun saat ini pendakian Gunung Arjun-Welirang telah dibuka, kata dia, namun antusiasme para pendaki masih minim. Tercatat baru ada 38 orang pendaki yang sudah melakukan pendakian ke gunung dengan ketinggian 3.339 meter di atas permukaan laut (mdpl).
+                            </p>
+                            <p class="blog-details__text">
+                                “Biasanya pendakian ke Arjuno itu ramai saat akhir pekan. Untuk hari-hari kerja seperti ini, sepi, bahkan tidak ada yang naik,” katanya.
+                            </p>
+                            <p class="blog-details__text">
+                                Menurutnya, para pendaki yang akan melakukan pendakian ke Gunung Arjuno-Welirang, harus sudah mendapatkan vaksinasi COVID-19. Para pendaki harus melakukan scan kode batang PeduliLindungi pada saat berada di base camp pintu masuk.
+                            </p>
+                            <p class="blog-details__text">
+                                Jika ada pendaki yang belum mendapatkan suntikan vaksin COVID-19, maka tidak diperbolehkan untuk melakukan pendakian. Selain itu, para calon pendaki juga harus menyertakan surat keterangan sehat pada saat akan memulai pendakian.
+                            </p>
+                            <p class="blog-details__text">
+                                “Selain harus sudah vaksin, calon pendaki juga harus menyertakan surat keterangan sehat saat akan memulai pendakian,” tambahnya.
+                            </p>
+                            <p class="blog-details__text">
+                                Kemudian, para pendaki harus memesan tiket melalui sistem online. Pengelola pendakian Gunung Arjuno-Welirang, tidak melayani pembelian tiket secara langsung. Pembelian tiket bisa dilakukan pada laman yang disiapkan oleh pengelola.
+                            </p>
+
+                            <p class="blog-details__text">
+                                “Jika ada yang datang tanpa memesan tiket online, bisa dipastikan akan ditolak, dan tidak dilayani,” katanya.
+                            </p>
+                            <p class="blog-details__text">
+                                Gunung Arjuno merupakan gunung tertinggi kedua di Jawa Timur, dengan ketinggian mencapai 3.339 meter di atas permukaan laut (mdpl). Sementara untuk Gunung Welirang, memiliki ketinggian 3.156 mdpl.
+                            </p>
+                            <p class="blog-details__text">
+                                Gunung Arjuno, terletak bersebelahan dengan Gunung Welirang, termasuk Gunung Kembar I, dan Gunung Kembar II. Puncak Gunung Arjuno terletak dalam satu punggungan yang sama dengan puncak Gunung Welirang, dan lebih dikenal sebagai kompleks Arjuno-Welirang.
+                            </p>
+                            <p class="blog-details__text">
+                                Sumber :
+                                https://radarsurabaya.jawapos.com/wisata-dan-kuliner/22/09/2021/pendakian-gunung-arjuno-dan-welirang-mulai-dibuka-untuk-masyarakat/
+                            </p>
+
+                            <br>
+                            <a href="../berita-all.html" class="banner-one__btn" style="display: table; margin: 0 auto">Berita Lainnya</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <?php include 'sidebar.html'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="site-footer" id="kontak" style="padding-top: 150px">
+            <div class="site-footer__main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget footer-widget--about-widget">
+                                <a href="../index.html" class="footer-widget__footer-logo"><img src="../images/logos/logos2.png" alt="Awesome Image" /></a>
+                                <p>Jl. Simpang Panji Suroso No.144, Arjosari, Kec. Blimbing, <br>
+                                    Kota Malang, Jawa Timur 65126</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Kontak
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>(0341) – 483254</li>
+                                    <li>tahuraradensoerjo@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Jam Operasional
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>Senin - Kamis, 07.00 - 15.30</li>
+                                    <li>Jumat, 07.00 - 14.30</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 d-flex">
+                            <div class="footer-widget my-auto">
+                                <div class="social-block">
+                                    <a target="_blank" href="https://instagram.com/tahuraradensoerjo.official?igshid=1lwx5yb6lhu3b"><i class="fab fa-instagram fa-2x"></i></a>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCl0gtcFpoFrQY6MU50tqC4Q?view_as=subscriber"><i class="fab fa-youtube fa-2x"></i></a>
+                                    <a target="_blank" href="mailto:tahuraradensoerjo@gmail.com?Subject=Hello"><i class="fas fa-envelope fa-2x"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="site-footer__bottom-footer text-center">
+                <div class="container">
+                    <p>&copy; copyright 2020 by <a href="index.html">UPT Tahura Raden Soerjo</a></p>
+                    <p>by <a target="_blank" href="https://www.instagram.com/amiruzg/">Amiruzzuhhad Gunes</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- /.scroll-to-top -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.fancybox.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <script src="../js/theme.js"></script>
+</body>
+
+</html>
---
+++ berita/berita-26.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tahura Raden Soerjo</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/logo-tahura.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+</head>
+
+<body>
+    <div class="preloader"></div><!-- /.preloader -->
+
+    <div class="page-wrapper">
+        <header class="site-header header-one header-one__home-two">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box clearfix">
+                        <a class="navbar-brand" href="../index.html">
+                            <img src="../images/logos/logos.svg" class="main-logo" alt="Awesome Image" />
+                        </a>
+                        <button class="menu-toggler" data-target=".header-one .main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box one-page-scroll-menu ">
+                            <li class="scrollToLink">
+                                <a href="../index.html">Beranda</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../index.html">Profil</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../sekilas.html">Pelajari Tahura</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../sekilas.html">Tugas dan Fungsi</a></li>
+                                    <li><a href="../organisasi.html">Struktur Organisasi</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.header-one -->
+
+        <section class="inner-banner">
+            <div class="container text-center">
+                <ul class="thm-breadcrumb">
+                    <li class="thm-breadcrumb__item"><a href="../index.html">Beranda</a></li>
+                    <li class="thm-breadcrumb__item"><span>Informasi</span></li>
+                </ul><!-- /.thm-breadcrumb -->
+            </div><!-- /.container -->
+        </section><!-- /.inner-banner -->
+
+        <section class="blog-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="blog-details__content">
+                            <div class="blog-details__date">7 Oktober 2021</div>
+                            <div class="blog-details__meta">
+                                <a href="#" class="blog-details__meta-link">CNN Indonesia</a>
+                            </div>
+                            <h3 class="blog-details__title">PPKM Level 3, Pendakian Gunung Arjuno-Welirang Ditutup Lagi</i></h3>
+                            <div class="blog-details__image" style="margin-bottom:30px">
+                                <img src="../images/berita/img-103.jpg" alt="Awesome Image" />
+                                <div class="sub-judul" style="padding-top:10px"><i>Gunung Arjuno</i></div>
+                            </div>
+                            <p class="blog-details__text">
+                                Pendakian di Gunung Arjuno dan Gunung Welirang yang ada di wilayah Jawa Timur kembali ditutup setelah sebelumnya dibuka di dua pintu masuk, yakni jalur Tambaksari, Kecamatan Purwodadi, dan Tretes, Kecamatan Prigen, Kabupaten Pasuruan. 
+                            </p>
+                            <p class="blog-details__text">
+                                Kepala Taman Hutan Raya (Tahura) Raden Soerjo, Ahmad Wahyudi, di Kota Malang, Jawa Timur, Rabu (6/10), mengatakan, pendakian Arjuno Welirang kembali ditutup karena di wilayah Kabupaten Pasuruan, saat ini menerapkan pemberlakuan pembatasan kegiatan masyarakat (PPKM) level 3.
+                            </p>
+                            <blockquote class="blockuot">
+                                "Kabupaten Pasuruan berada pada level 3 PPKM, di mana kegiatan wisata tidak diizinkan. Maka, pendakian Arjuno Welirang ditutup," kata Wahyudi seperti yang dikutip dari ANTARA pada Kamis (7/10).
+                            </blockquote>
+                            <p class="blog-details__text">
+                                Penutupan pendakian yang baru saja dibuka pada 19 September 2021 tersebut, terhitung sejak 5 Oktober 2021, hingga waktu yang belum ditentukan. Penutupan tersebut, mengacu pada Instruksi Menteri Dalam Negeri Nomot 47 Tahun 2021.
+                            </p>
+                            <p class="blog-details__text">
+                                Penutupan tersebut tertuang dalam pengumuman yang dikeluarkan oleh UPT Tahura Raden Soerjo Nomor 522/685/123.7/2021 Tentang Penutupan Kegiatan Pendakian Gunung Arjuno Welirang Melalui Jalur Tretes, dan Tambaksari.
+                            </p>
+                            <p class="blog-details__text">
+                                "Kegiatan pendakian Gunung Arjuno Welirang, akan dibuka kembali dengan memperhatikan instruksi pemerintah lebih lanjut," ujarnya.
+                            </p>
+                            <p class="blog-details__text">
+                                Gunung Arjuno secara administratif terletak di perbatasan Kota Batu, Kabupaten Malang, dan Kabupaten Pasuruan. Tiga wilayah tersebut, saat ini masih menerapkan PPKM level 3, sehingga pendakian Arjuno Welirang harus ditutup.
+                            </p>
+                            <p class="blog-details__text">
+                                Gunung Arjuno merupakan gunung tertinggi kedua di Jawa Timur, dengan ketinggian mencapai 3.339 meter di atas permukaan laut (mdpl). Sementara untuk Gunung Welirang, memiliki ketinggian 3.156 mdpl.
+                            </p>
+                            <p class="blog-details__text">
+                                Gunung Arjuno, terletak bersebelahan dengan Gunung Welirang, termasuk Gunung Kembar I, dan Gunung Kembar II.
+                            </p>
+                            <p class="blog-details__text">
+                                Puncak Gunung Arjuno terletak dalam satu punggungan yang sama dengan puncak Gunung Welirang, dan lebih dikenal sebagai kompleks Arjuno-Welirang.
+                            </p>
+
+                            <br>
+                            <a href="../berita-all.html" class="banner-one__btn" style="display: table; margin: 0 auto">Berita Lainnya</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <?php include 'sidebar.html'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="site-footer" id="kontak" style="padding-top: 150px">
+            <div class="site-footer__main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget footer-widget--about-widget">
+                                <a href="../index.html" class="footer-widget__footer-logo"><img src="../images/logos/logos2.png" alt="Awesome Image" /></a>
+                                <p>Jl. Simpang Panji Suroso No.144, Arjosari, Kec. Blimbing, <br>
+                                    Kota Malang, Jawa Timur 65126</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Kontak
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>(0341) – 483254</li>
+                                    <li>tahuraradensoerjo@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Jam Operasional
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>Senin - Kamis, 07.00 - 15.30</li>
+                                    <li>Jumat, 07.00 - 14.30</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 d-flex">
+                            <div class="footer-widget my-auto">
+                                <div class="social-block">
+                                    <a target="_blank" href="https://instagram.com/tahuraradensoerjo.official?igshid=1lwx5yb6lhu3b"><i class="fab fa-instagram fa-2x"></i></a>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCl0gtcFpoFrQY6MU50tqC4Q?view_as=subscriber"><i class="fab fa-youtube fa-2x"></i></a>
+                                    <a target="_blank" href="mailto:tahuraradensoerjo@gmail.com?Subject=Hello"><i class="fas fa-envelope fa-2x"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="site-footer__bottom-footer text-center">
+                <div class="container">
+                    <p>&copy; copyright 2020 by <a href="index.html">UPT Tahura Raden Soerjo</a></p>
+                    <p>by <a target="_blank" href="https://www.instagram.com/amiruzg/">Amiruzzuhhad Gunes</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- /.scroll-to-top -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.fancybox.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <script src="../js/theme.js"></script>
+</body>
+
+</html>
