7a9caf856cd0bf3dbc27528aad7c72d22beb549e
amiruzg134 <amiruzg@gmail.com>
2022-03-21 14:48:58 +0700

    MSI

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 $conn = new mysqli('localhost', 'root', '', 'dishut');
 // $conn = new mysqli('tahuraradensoerjo.or.id', 'tahurara_tahura', 'amiruzg627408', 'tahurara_tahura');
+// $conn = mysqli_connect("localhost","tahurars_tahurarsoerjo","Xtahura456","tahurars_tahura");
 
 // Check connection
 if (!$conn) {
