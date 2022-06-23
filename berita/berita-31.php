92cd002205262e5cf427a74bb559ced7931f6b26
amiruzg134 <amiruzg@gmail.com>
2022-06-23 08:57:54 +0700

    msi

---
+++ berita/berita-31.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tahura Raden Soerjo</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/logos/logo-tahura.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="../font-awesome/css/all.css">
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/owl.theme.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+</head>
+
+<body>
+    <div class="preloader"></div><!-- /.preloader -->
+
+    <div class="page-wrapper">
+        <header class="site-header header-one header-one__home-two">
+            <nav class="navbar navbar-expand-lg navbar-light header-navigation stricky">
+                <div class="container clearfix">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="logo-box clearfix">
+                        <a class="navbar-brand" href="../index.html">
+                            <img src="../images/logos/logos.svg" class="main-logo" alt="Awesome Image" />
+                        </a>
+                        <button class="menu-toggler" data-target=".header-one .main-navigation">
+                            <span class="fa fa-bars"></span>
+                        </button>
+                    </div><!-- /.logo-box -->
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="main-navigation">
+                        <ul class=" navigation-box one-page-scroll-menu ">
+                            <li class="scrollToLink">
+                                <a href="../index.html">Beranda</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../index.html">Profil</a>
+                            </li>
+                            <li class="scrollToLink">
+                                <a href="../sekilas.html">Pelajari Tahura</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../sekilas.html">Tugas dan Fungsi</a></li>
+                                    <li><a href="../organisasi.html">Struktur Organisasi</a></li>
+                                </ul><!-- /.sub-menu -->
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container -->
+            </nav>
+        </header><!-- /.header-one -->
+
+        <section class="inner-banner">
+            <div class="container text-center">
+                <ul class="thm-breadcrumb">
+                    <li class="thm-breadcrumb__item"><a href="../index.html">Beranda</a></li>
+                    <li class="thm-breadcrumb__item"><span>Berita</span></li>
+                </ul><!-- /.thm-breadcrumb -->
+            </div><!-- /.container -->
+        </section><!-- /.inner-banner -->
+
+        <section class="blog-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="blog-details__content">
+                            <div class="blog-details__date">17 Mei 2022</div>
+                            <div class="blog-details__meta">
+                                <a href="#" class="blog-details__meta-link">VENDA AYUNG P.</a>
+                            </div>
+                            <h3 class="blog-details__title">PEMBINAAN GENERASI MUDA PECINTA ALAM</i></h3>
+                            <p class="blog-details__text">
+                                Salah satu cara menjaga kawasan Tahura Raden Soerjo dapat dilakukan melalui pencegahan perusakan ekosistem oleh masyarakat di sekitar kawasan. Sejalan dengan maksud tersebut UPT Tahura Raden Soerjo melakukan upaya pembinaan generasi muda pecinta alam dengan melakukan pendidikan cinta lingkungan. Melalui Kegiatan ini diharapkan mampu membentuk karakter diri dengan pendidikan lingkungan di kalangan pelajar yang nantinya dapat menjadi agen-agen untuk menyebarluaskan dan mengajak orang-orang di sekitar mereka untuk dapat lebih mencintai lingkungan.
+                            </p>
+                            <p class="blog-details__text">
+                                Kegiatan ini difokuskan untuk memberikan arahan dan mengembangkan wawasan generasi muda melalui sekolah-sekolah. Adapun sekolah yang terlibat yaitu SMPN 1 Wonosalam, Kecamatan Wonosalam, Kabupaten Jombang. Dengan adanya pembinaan generari muda pecinta alam bertujuan untuk lebih menanamkan rasa cinta terhadap lingkungan serta memberikan pemahaman agar senantiasa berpegang pada dasar-dasar konservasi baik dalam kehidupan sehari-hari maupun dalam berkegiatan. 
+                            </p>
+                            <p class="blog-details__text">
+                                Kegiatan yang dilaksanakan pada tanggal 17 Mei 2022 ini melibatkan Penyuluh Kehutanan, Pengendali Ekosistem Hutan dan Polisi Kehutanan lingkup UPT Tahura Raden Soerjo untuk turut serta berbagi pengetahuan, pengalaman dan mendorong peserta untuk sadar konservasi dalam kehidupan sehari-hari.
+                            </p>
+                            <p class="blog-details__text">
+                                Pemateri pembinaan generasi muda pecinta alam berjumlah 5 orang dari lingkup Tahura Raden Soerjo, antara lain adalah pengetahuan tentang Tahura Raden Soerjo, Potensi Flora di Tahura Raden Soerjo, Potensi Fauna di Tahura Raden Soerjo, Manfaat Pohon dan Rantai Makanan.
+                            </p>
+                            <p class="blog-details__text">
+                                Keberadaan Taman Hutan Raya Raden Soerjo akan dapat terjaga dan lestari dengan adanya masyarakat – masyarakat yang sadar konservasi  yang berada disekitar kawasan tahura. Hal ini dikarenakan keterbatasan sumberdaya yang ada di Tahura Raden Soerjo sehingga dibutuhkan peran aktif dari masyarakat disekitar kawasan. Kegiatan pembinaan generasi muda pecinta alam bagi pelajar di kawasan Tahura Raden Soerjo diharapkan dapat menjadi momentum untuk meningkatkan kesadaran dan semangat cinta lingkungan terhadap kawasan tahura. Dari peserta diharapkan muncul cikal bakal agen konservasi yang dapat membantu menyebarluaskan arti penting Tahura Raden Soerjo.
+                            </p>
+                            <div class="blog-details__image" style="margin:30px 0">
+                                <img src="../images/berita/img-119.jpg" alt="Awesome Image" />
+                            </div>
+                            <div class="blog-details__image" style="margin:30px 0">
+                                <img src="../images/berita/img-120.jpg" alt="Awesome Image" />
+                            </div>
+                            <br>
+                            <a href="../berita-all.html" class="banner-one__btn" style="display: table; margin: 0 auto">Berita Lainnya</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <?php include 'sidebar.html'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="site-footer" id="kontak" style="padding-top: 150px">
+            <div class="site-footer__main-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget footer-widget--about-widget">
+                                <a href="../index.html" class="footer-widget__footer-logo"><img src="../images/logos/logos2.png" alt="Awesome Image" /></a>
+                                <p>Jl. Simpang Panji Suroso No.144, Arjosari, Kec. Blimbing, <br>
+                                    Kota Malang, Jawa Timur 65126</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Kontak
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>(0341) – 483254</li>
+                                    <li>tahuraradensoerjo@gmail.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12">
+                            <div class="footer-widget">
+                                <h3 class="footer-widget__title">
+                                    Jam Operasional
+                                </h3>
+                                <ul class="footer-widget__link-list">
+                                    <li>Senin - Kamis, 07.00 - 15.30</li>
+                                    <li>Jumat, 07.00 - 14.30</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 d-flex">
+                            <div class="footer-widget my-auto">
+                                <div class="social-block">
+                                    <a target="_blank" href="https://instagram.com/tahuraradensoerjo.official?igshid=1lwx5yb6lhu3b"><i class="fab fa-instagram fa-2x"></i></a>
+                                    <a target="_blank" href="https://www.youtube.com/channel/UCl0gtcFpoFrQY6MU50tqC4Q?view_as=subscriber"><i class="fab fa-youtube fa-2x"></i></a>
+                                    <a target="_blank" href="mailto:tahuraradensoerjo@gmail.com?Subject=Hello"><i class="fas fa-envelope fa-2x"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="site-footer__bottom-footer text-center">
+                <div class="container">
+                    <p>&copy; copyright 2020 by <a href="index.html">UPT Tahura Raden Soerjo</a></p>
+                    <p>by <a target="_blank" href="https://www.instagram.com/amiruzg/">Amiruzzuhhad Gunes</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <!-- /.scroll-to-top -->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="../js/jquery.fancybox.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.min.js"></script>
+    <script src="../js/jquery.bxslider.min.js"></script>
+    <script src="../js/theme.js"></script>
+</body>
+
+</html>
